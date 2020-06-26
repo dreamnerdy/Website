70d72c5c4ea63e4ce96979ff32386fabd99e93b9
Bearilian <57048330+Bearilian@users.noreply.github.com>
2020-06-26 13:18:50 -0400

    revert

---
+++ dreamnerdy/_site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Disc Golf, Off The Chains",
+        "excerpt":"“Virtual Reality Disc Golf! Coming Soon!!!” Dream Nerdy is proud to announce the production of our indie game Disc Golf, Off The Chains, which will be an immersive light hearted Virtual Disc Golf experience. Please enjoy this mini teaser and stay tuned for development videos and updates. As our first...","categories": ["game,","vr,","disc-golf"],
+        "tags": [],
+        "url": "http://localhost:4000/game,/vr,/disc-golf/2020/06/14/dream-nerdy.html",
+        "teaser": null
+      }]
